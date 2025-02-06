803f3b6cb70f0899499ea594cb8a45d1be61bc9e
[nareshbabumandula] <naresh223@gmail.com>
2025-02-06 17:24:08 +0530

    06-Feb-2025 - Added EGW Store Search scenarios with City, State and Zipcode

--- Playwright_BDD_Automation/tests/features/EGWStoreSearch.feature
+++ Playwright_BDD_Automation/tests/features/EGWStoreSearch.feature
@@ -1,10 +1,26 @@
-@smoke
 Feature: Store Search Functionality
 
 Background:
 Given I access eyeglassworld portal 
 
-@smoke
 Scenario: Verify the functionality of searching an eyeglass store based on 'City' name
 When I search for an eyeglass store based on City as 'tampa'
 Then I should see the appropriate store results
+
+Scenario: Verify the functionality of searching an eyeglass store based on 'State' name
+When I search for an eyeglass store based on State as 'florida'
+Then I should see the appropriate store results
+
+Scenario: Verify the functionality of searching an eyeglass store based on 'zipcode'
+When I search for an eyeglass store based on zipcode as '33629'
+Then I should see the appropriate store results
+
+@smoke
+Scenario Outline: Verify the functionality of searching an eyeglass store based on 'city/state/zipcode'
+When I search for an eyeglass store based on '<searchType>' as '<searchValue>'
+Then I should see the '<searchValue>' in store search results 
+Examples:
+|searchType|searchValue|
+|City|tampa|
+|State|florida|
+|Zipcode|33629|
--- Playwright_BDD_Automation/tests/steps/StoreSearch.js
+++ Playwright_BDD_Automation/tests/steps/StoreSearch.js
@@ -21,13 +21,37 @@ When("I search for an eyeglass store based on City as {string}", async function 
     console.log(`Searched the store based on city name: ${city}`);
 });
 
+When('I search for an eyeglass store based on State as {string}', async function (state) {
+    await this.page.locator('id=inputStoreValue').fill(state);
+    await this.page.waitForTimeout(2000);
+    await this.page.locator('//button[contains(text(),"Find a Store")]').click();
+    await this.page.waitForTimeout(5000);
+    console.log(`Searched the store based on state name: ${state}`);
+});
+
+When('I search for an eyeglass store based on zipcode as {string}', async function (zipcode) {
+    await this.page.locator('id=inputStoreValue').fill(zipcode);
+    await this.page.waitForTimeout(2000);
+    await this.page.locator('//button[contains(text(),"Find a Store")]').click();
+    await this.page.waitForTimeout(5000);
+    console.log(`Searched the store based on state name: ${zipcode}`);
+  });
+
+  When('I search for an eyeglass store based on {string} as {string}', async function (searchType, searchValue) {
+    await this.page.locator('id=inputStoreValue').fill(searchValue);
+    await this.page.waitForTimeout(2000);
+    await this.page.locator('//button[contains(text(),"Find a Store")]').click();
+    await this.page.waitForTimeout(5000);
+    console.log(`Searching for eyeglass store by ${searchType}: ${searchValue}`);
+  });
+
 Then("I should see the appropriate store results", async function () {
     await this.page.waitForSelector('text=tampa'); 
     await this.page.locator('text=tampa') // Check of tampa is found in the results
     await this.page.locator('text=The Plaza at Estrella'); // Verify the store name exists
     await this.page.locator('text=Open Now'); // Verify the store status displayed
     const scheduleExam = await this.page.locator('text=Schedule Exam'); 
-    const visitStorePage = await this.page.locator('text=    Visit Store Page'); 
+    const visitStorePage = await this.page.locator('text=Visit Store Page'); 
 
     // Verfify Schedule exam and Visit store page buttons displayed
     await expect(scheduleExam).toBeVisible();
@@ -35,3 +59,16 @@ Then("I should see the appropriate store results", async function () {
     
     console.log("Successfully verified the store search results..!");
 });
+
+
+Then('I should see the {string} in store search results', async function (searchValue) {
+    await this.page.waitForSelector(`text=${searchValue}`); 
+    await this.page.locator('text=Open Now'); // Verify the store status displayed
+    const scheduleExam = await this.page.locator('text=Schedule Exam'); 
+    const visitStorePage = await this.page.locator('text=Visit Store Page'); 
+
+    // Verfify Schedule exam and Visit store page buttons displayed
+    await expect(scheduleExam).toBeVisible();
+    await expect(visitStorePage).toBeVisible();
+});
+
